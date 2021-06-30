c6e7e4ed1a2c10bdc0d590cffcba837c4bc835bb
Aadesh Gujjar <34857535+AadeshGujjar@users.noreply.github.com>
2021-07-01 00:01:40 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 const DEVELOPMENT_URL= "http://localhost:5402";
-const PRODUCTION_URL= "https://zomatoaadesh.herokuapp.com/";
+const PRODUCTION_URL= "https://zomatoaadesh.herokuapp.com";
 exports.API_URL=PRODUCTION_URL;
