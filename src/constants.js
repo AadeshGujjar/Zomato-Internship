cb709f08e443f2d1ab42ed7e05917414570a043d
Aadesh Gujjar <34857535+AadeshGujjar@users.noreply.github.com>
2021-07-01 00:10:07 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 const DEVELOPMENT_URL= "http://localhost:5402";
-const PRODUCTION_URL= "https://git.heroku.com/boiling-falls-77601.com";
+const PRODUCTION_URL= "https://www.heroku.com/boiling-falls-77601.com";
 exports.API_URL=PRODUCTION_URL;
